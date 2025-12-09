c47e3c184d58fb70d498af7e723478acef793521
nischaljs <nishchalb21@gmail.com>
2025-12-09 12:16:51 +0545

    basics of js learned about this and that

---
+++ jsbasics/index.js
@@ -0,0 +1,31 @@
+
+
+// let person = {
+//     name: "Nischal",
+//     age: 12,
+//     class: "IV"
+// }
+
+// console.log(person.name)
+
+
+// let nameOfAnimal = ["dog", "cat", "rabbit"];   //3     //2
+
+// console.log(nameOfAnimal[2]);
+
+// for(let i = 0; i<=nameOfAnimal.length;i++){
+//     console.log(nameOfAnimal[i])
+// }
+
+
+//concatenation
+
+let a = 50;
+
+console.log("I am "+a + "fsdkgfhsgf0 + a" +a )
+
+
+//template literals
+
+let name = "ram";
+console.log(`my name is ${name} name ${name}`)
